b29ad0ec730f9f4671ff1a095509ceaf616abdd7
Tuva <tuva.serigstad@icloud.com>
2025-03-25 14:59:12 +0100

    Describe the changes you made here

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,11 @@
 import numpy as np
 
 class LogisticRegression:
-    
+    def __init__(self, learning_rate=0.5, epochs=1000):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights, self.bias = None, None
+        self.losses, self.train_accuacies = [], []
+
+    def sigmoid_function(self, X):
+        return X
