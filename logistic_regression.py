ef5160835a2387e1cacb7fab6b3defbaad25150d
Tuva <tuva.serigstad@icloud.com>
2025-03-25 16:23:07 +0100

    endringer

--- logistic_regression.py
+++ logistic_regression.py
@@ -19,18 +19,21 @@ def sigmoid_function(z):
 
 def train_linear_classifier(X, T, alpha=0.5, max_iter=1000, tol=1e-5):
     N, D = X.shape
-    C = T.shape[0]
+    C = T.shape[1]
     W = np.zeros((C, D))
     mse_history = []
 
     for epoch in range(max_iter):
-        Z = X @ np.transpose(W)
+        Z = X @ W.T
         G = sigmoid_function(Z)
         E = G - T
         mse = np.mean(np.sum(E**2, axis=1))
         mse_history.append(mse)
 
-        grad = np.sum((E*G*(1-G))*np.transpose(X))
+        grad = (E*G*(1-G)).T@X
         W -= alpha*grad
 
     return W, mse_history
+
+def predict(X, W):
+    lin_model = W@X
